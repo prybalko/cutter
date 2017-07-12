c94664b9d2b101ee867bcd7aacc4412ed8b63ac6
Pavel Rybalko <paul.rybalko@gmail.com>
2017-07-12 21:34:19 +0700

    use hash in base 62

--- cutter/lib.py
+++ cutter/lib.py
@@ -4,7 +4,7 @@ import hashlib
 from cutter import db
 from cutter.models import Link
 
-MIN_URL_LENGTH = 6
+MIN_URL_LENGTH = 5
 
 
 def get_object_or_404(model, pk):
@@ -14,11 +14,16 @@ def get_object_or_404(model, pk):
     return obj
 
 
-def shorten_url(long_url):
-    full_hash = short_url = hashlib.sha1(long_url).hexdigest()
+def base_n(num, b, numerals="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+    return ((num == 0) and numerals[0]) or (base_n(num // b, b, numerals).lstrip(numerals[0]) + numerals[num % b])
 
-    for url_length in xrange(MIN_URL_LENGTH, len(full_hash)+1):
-        short_url = full_hash[:url_length]
+
+def shorten_url(long_url):
+    hex_hash = hashlib.sha1(long_url).hexdigest()
+    _hash = short_url = base_n(int(hex_hash, 16), 62)
+
+    for url_length in xrange(MIN_URL_LENGTH, len(_hash)+1):
+        short_url = _hash[:url_length]
         link = Link.query.get(short_url)
         if not link:
             new_link = Link(short_url=short_url, long_url=long_url)
--- cutter/models.py
+++ cutter/models.py
@@ -2,8 +2,8 @@ from cutter import db
 
 
 class Link(db.Model):
-    short_url = db.Column(db.String(40), primary_key=True)
-    long_url = db.Column(db.String(2000), unique=True)
+    short_url = db.Column(db.String, primary_key=True)
+    long_url = db.Column(db.String, unique=True)
 
     def __init__(self, short_url, long_url):
         self.short_url = short_url
